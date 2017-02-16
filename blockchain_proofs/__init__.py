d96af2a39b88b541bbe9e89e420de046d53626f4
karask <kkarasavvas@gmail.com>
2017-02-16 15:30:29 +0200

    added internal import and another requirement

--- blockchain_proofs/__init__.py
+++ blockchain_proofs/__init__.py
@@ -1,1 +1,3 @@
+from .chainpoint import ChainPointV2
+
 __version__ = '0.1.0'
--- blockchain_proofs/chainpoint.py
+++ blockchain_proofs/chainpoint.py
@@ -1,4 +1,5 @@
 import glob
+import requests
 import hashlib
 from merkletools import MerkleTools
 
@@ -11,7 +12,7 @@ CHAINPOINT_HASH_TYPES = {'sha224': 'ChainpointSHA224v2',
                          'sha3_256': 'ChainpointSHA3-256v2',
                          'sha3_384': 'ChainpointSHA3-384v2',
                          'sha3_512': 'ChainpointSHA3-512v2' }
-CHAINPOINT_ANCHOR_TYPES = {'btc': 'BTCOpReturn', 'eth': 'ETHData'}
+CHAINPOINT_ANCHOR_TYPES = {'btc': 'BTCOpReturn'} #, 'eth': 'ETHData'}
 
 '''
 Implements chainpoint v2 proof of existence approach
@@ -65,38 +66,87 @@ class ChainPointV2(object):
     Currently only works for BTC anchors
     '''
     def get_receipt(self, index, btc_source_id):
-        return {
-            "@context": CHAINPOINT_CONTEXT,
-            "type": self.get_chainpoint_hash_type(),
-            "targetHash": self.get_leaf(index),
-            "merkleRoot": self.get_merkle_root(),
-            "proof": self.get_proof(index),
-            "anchors": [
-                {
-                    "type": "BTCOpReturn",
-                    "sourceId": btc_source_id
-                }
-            ]
-        }
+        if self.get_tree_ready_state():
+            return {
+                "@context": CHAINPOINT_CONTEXT,
+                "type": self.get_chainpoint_hash_type(),
+                "targetHash": self.get_leaf(index),
+                "merkleRoot": self.get_merkle_root(),
+                "proof": self.get_proof(index),
+                "anchors": [
+                    {
+                        "type": "BTCOpReturn",
+                        "sourceId": btc_source_id
+                    }
+                ]
+            }
+        else:
+            return None
 
 
-    '''Validates a chainpoint receipt. Currently only for BTC anchors'''
-    def validate_receipt(self, receipt):
+    '''
+    Validates a chainpoint receipt. Currently only for BTC anchors
+        receipt is the chainpoint_proof metadata from the pdf file.
+        certificate_hash is the hash of the certificate after we removed the
+            chainpoint_proof metadata
+        metadata_prefix is the prefix chosen when issuing in the blockchain
+        testnet specifies if testnet or mainnet was used
+    '''
+    def validate_receipt(self, receipt, certificate_hash, metadata_prefix='', testnet=False):
         # check context and hash type
-        if(receipt['@context'] != CHAINPOINT_CONTEXT):
+        if(receipt['@context'].lower() != CHAINPOINT_CONTEXT):
             return False
         if(receipt['type'] not in CHAINPOINT_HASH_TYPES.values()):
             return False
         target_hash = receipt['targetHash']
         merkle_root = receipt['merkleRoot']
         proof = receipt['proof']
+
+        # validate actual hash
+        if target_hash.lower() != certificate_hash.lower():
+            return False
+
         # validate merkle proof
         if(not self.validate_proof(proof, target_hash, merkle_root)):
            return False
 
+        # get anchor
+        # TODO currently gets only the first valid (BTC) anchor
+        anchors = receipt['anchors']
+        txid = ''
+        for a in anchors:
+            if a['type'] in CHAINPOINT_ANCHOR_TYPES.values():
+                txid = a['sourceId']
+                break
+
         # validate anchor
+        hash_hex = self.fetch_op_return_file_hash(txid, metadata_prefix, testnet)
+        if(not merkle_root.lower() == hash_hex.lower()):
+            return False
 
         return True
+
+
+    def fetch_op_return_file_hash(self, txid, metadata_prefix='', testnet=False):
+        if testnet:
+            blockr_url = "http://tbtc.blockr.io/api/v1/tx/info/" + txid
+        else:
+            blockr_url = "http://btc.blockr.io/api/v1/tx/info/" + txid
+
+        response = requests.get(blockr_url).json()
+        vouts = response['data']['vouts']
+        hash_hex = ""
+        for o in vouts:
+            script = o['extras']['script']
+            if script.startswith('6a'):
+                asm = o['extras']['asm']
+                if metadata_prefix:
+                    split_asm = " " + metadata_prefix
+                else:
+                    split_asm = " "
+                hash_hex = asm.split(split_asm, 1)[1]
+                break
+        return hash_hex
 
 
 def main():
@@ -114,6 +164,7 @@ def main():
     r = cp.get_receipt(0, 'aaaaaddddaaa4a4a')
     print(r)
     print ( cp.validate_receipt(r) )
+    print ( cp.fetch_op_return_file_hash('509f6b149d71661aa803ebf13c7f6f55a4fece961982fca2d72d5a084968b8b1', '554e6963444320'))
     #ADD TESTS TO PROJECT !!!!!!!!!!!!!
 
 if __name__ == "__main__":
